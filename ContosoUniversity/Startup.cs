dd45920a01feaa31244f685b33285720bc728d69
Joe <overtonje@gmail.com>
2021-01-12 20:53:45 -0600

    added dbinitializer and scaffolded student controller and views

--- ContosoUniversity/Startup.cs
+++ ContosoUniversity/Startup.cs
@@ -8,6 +8,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContosoUniversity.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
 
 namespace ContosoUniversity
 {
@@ -24,6 +27,8 @@ namespace ContosoUniversity
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<SchoolContext>(options =>
+    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
